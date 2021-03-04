4890ffcbba5260ce2f4d26a8c1dc0732013c5586
nikhilsharma10 <nikhil12.sharma@nttdata.com>
2021-03-04 15:02:57 +0000

    Config changes

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://exercise-3.vercel.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://exercise-3-3q8gupo3o-nikhilsharma10.vercel.app'
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     images: {
       domains: ['cdn.shopify.com'],
+      loader: "imgix",
     },
   }
